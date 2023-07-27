feda95b136d2de57a3c6c7c7a46274e126376bc3
bmcgauley <brian@mcgauley.com>
2023-07-27 11:21:35 -0700

    tester

--- public/solver.js
+++
@@ -1,8 +0,0 @@
-import captcha from "2captcha";
-const solver = new captcha.Solver(process.env.CAPTCHA_API_KEY);
-
-export default async (siteKey, url) => {
-  const obj = await solver.hcaptcha(siteKey, url);
-  const { data } = obj;
-  return data;
-};
